f1213d9565ef66bc6086baea7ff0a9e613eea775
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2016-07-14 01:02:57 +0200

    Building utility matrix

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import csv
 
 import click
@@ -14,6 +15,33 @@ def read_ratings_from_csv(input_file):
             user_movie.append([user_id, movie_id])
             ratings.append(rating)
     return np.array(user_movie), np.array(ratings).astype(np.int32)
+
+
+def build_user_utility_matrix(input_file):
+    user_ratings = defaultdict(dict)
+    last_user_id = 0
+    last_movie_id = 0
+    with open(input_file, "r") as input:
+        reader = csv.reader(input)
+        for row in reader:
+            user_id, movie_id, rating, _timestamp = row
+            user_id = int(user_id) - 1
+            movie_id = int(movie_id) - 1
+            last_user_id = max(last_user_id, user_id)
+            last_movie_id = max(last_movie_id, movie_id)
+            user_ratings[user_id][movie_id] = rating
+    user_ratings_list = user_ratings.items()
+    user_utility_matrix = np.zeros((last_user_id + 1, last_movie_id + 1))
+    for user_id, ratings in user_ratings_list:
+        for movie_id, rating in ratings.iteritems():
+            user_utility_matrix[user_id, movie_id] = rating
+    print user_utility_matrix.shape
+    print user_utility_matrix[0]
+    return user_utility_matrix
+
+
+def build_item_utility_matrix(input_file):
+    return build_user_utility_matrix(input_file).transpose()
 
 
 @click.command()
