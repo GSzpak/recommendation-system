0b53ce56a2b27bf0e7d6fa70e26563649844d8ac
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2016-07-21 13:17:46 +0200

    Collaborative filtering predictors added

---
+++ collaborative_filtering.py
@@ -0,0 +1,124 @@
+from collections import defaultdict
+import itertools
+
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sortedcontainers import SortedSet
+
+from utils import nonzero_mean
+
+
+def _build_user_utility_matrix(X, y):
+    user_ratings = defaultdict(dict)
+    last_user_id = 0
+    last_movie_id = 0
+    for (user_id, movie_id), rating in itertools.izip(X, y):
+        user_id = int(user_id) - 1
+        movie_id = int(movie_id) - 1
+        last_user_id = max(last_user_id, user_id)
+        last_movie_id = max(last_movie_id, movie_id)
+        user_ratings[user_id][movie_id] = rating
+    user_ratings_list = user_ratings.items()
+    user_utility_matrix = np.zeros((last_user_id + 1, last_movie_id + 1), dtype=np.int32)
+    for user_id, ratings in user_ratings_list:
+        for movie_id, rating in ratings.iteritems():
+            user_utility_matrix[user_id, movie_id] = rating
+    return user_utility_matrix
+
+
+def _build_items_utility_matrix(X, y):
+    return _build_user_utility_matrix(X, y).transpose()
+
+
+class CollaborativeFilteringPredictor(BaseEstimator, ClassifierMixin):
+
+    def __init__(self, k, similarity_measure):
+        self.utility_matrix = None
+        self.k = k
+        self.similarity_measure = similarity_measure
+        self._neighbours = {}
+        self.mean_rating = 0.0
+
+    def _build_utility_matrix(self, X, y):
+        raise NotImplementedError
+
+    def _find_nearest_neighbours(self, id_, ratings):
+        neighbours = SortedSet()
+        for neighbour, neighbour_ratings in enumerate(self.utility_matrix):
+            if neighbour == id_:
+                continue
+            similarity = self.similarity_measure(ratings, neighbour_ratings)
+            if len(neighbours) < self.k:
+                neighbours.add((similarity, neighbour))
+            else:
+                smallest_similarity = neighbours[0][1]
+                if smallest_similarity < similarity:
+                    neighbours.pop(0)
+                    neighbours.add((similarity, neighbour))
+        return neighbours
+
+    def fit(self, X, y):
+        self._build_utility_matrix(X, y)
+        for id_, ratings in enumerate(self.utility_matrix):
+            self._neighbours[id_] = self._find_nearest_neighbours(id_, ratings)
+        self.mean_rating = np.mean(y)
+
+    def get_prediction_from_neighbours(self, id_, col_index):
+        if id_ not in self._neighbours:
+            return self.mean_rating
+        neighbours = self._neighbours[id_]
+        numerator = 0.0
+        denominator = 0.0
+        for similarity, neighbour_id in neighbours:
+            neighbour_rating = self.utility_matrix[neighbour_id, col_index]
+            numerator += similarity * neighbour_rating
+            denominator += np.abs(similarity)
+        return numerator / denominator
+
+    def predict(self, X):
+        raise NotImplementedError
+
+
+class UserUserCollaborativeFilteringPredictor(CollaborativeFilteringPredictor):
+
+    def _build_utility_matrix(self, X, y):
+        self.utility_matrix = _build_user_utility_matrix(X, y)
+
+    def predict(self, X):
+        predictions = []
+        for user_id, movie_id in X:
+            user_id = int(user_id) - 1
+            movie_id = int(movie_id) - 1
+            predictions.append(self.get_prediction_from_neighbours(user_id, movie_id))
+        return np.asarray(predictions)
+
+
+class ModifiedUserUserCollaborativeFilteringPredictor(UserUserCollaborativeFilteringPredictor):
+
+    def get_prediction_from_neighbours(self, user_id, item_id):
+        if user_id not in self._neighbours:
+            return self.mean_rating
+        neighbours = self._neighbours[user_id]
+        user_mean = nonzero_mean(self.utility_matrix[user_id])
+        numerator = 0.0
+        denominator = 0.0
+        for similarity, neighbour_id in neighbours:
+            neighbour_rating = self.utility_matrix[neighbour_id, item_id]
+            neighbour_mean = nonzero_mean(self.utility_matrix[neighbour_id])
+            numerator += similarity * (neighbour_rating - neighbour_mean)
+            denominator += np.abs(similarity)
+        return user_mean + (numerator / denominator)
+
+
+class ItemItemCollaborativeFilteringPredictor(CollaborativeFilteringPredictor):
+
+    def _build_utility_matrix(self, X, y):
+        self.utility_matrix = _build_items_utility_matrix(X, y)
+
+    def predict(self, X):
+        predictions = []
+        for user_id, movie_id in X:
+            user_id = int(user_id) - 1
+            movie_id = int(movie_id) - 1
+            predictions.append(self.get_prediction_from_neighbours(movie_id, user_id))
+        return np.asarray(predictions)
